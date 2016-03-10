a9a2538aaf7c71805473df1ee6048624e8636a6d
Nicky Semenza <14nicholasse@gmail.com>
2016-03-10 03:03:46 -0500

    hacker list links to application detail state + button to go to a random application detail for rating purposes

--- src/app/exec/exec.controller.js
+++ src/app/exec/exec.controller.js
@@ -1,7 +1,17 @@
 'use strict';
 angular.module('app')
-.controller('ExecController', ['$rootScope', '$scope', 'ApiRest','ngToast', 'urls', function ($rootScope, $scope,  ApiRest,ngToast, urls) {
+.controller('ExecController', ['$rootScope', '$scope', 'ApiRest','ngToast', 'urls','$state',
+ function ($rootScope, $scope,  ApiRest,ngToast, urls, $state) {
   ApiRest.one('execs/hackers').get().then(function(data) {
       $scope.hackers = data;
     });
+  $scope.rateApplications = function()
+  {
+  	ApiRest.one('execs/applications/next').get().then(function(data) {
+      if(data)
+        	$state.go('exec-application-detail', {id:data});
+        else
+        	$state.go('exec');
+    });
+  };
   }]);
