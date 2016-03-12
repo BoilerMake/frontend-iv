215597a5b53b3b029f0ddbc0eae9c952812741b3
Nicky Semenza <14nicholasse@gmail.com>
2016-03-11 21:04:36 -0500

    automatically save applications

--- src/app/account/account.controller.js
+++ src/app/account/account.controller.js
@@ -25,22 +25,39 @@ angular.module('app')
       })
     }
   }])
-  .controller('ApplicationController', ['$rootScope', '$scope', 'ApiRest','ngToast', 'urls', function ($rootScope, $scope,  ApiRest,ngToast, urls) {
-  ApiRest.one('users/me/application').get().then(function(data) {
+  .controller('ApplicationController', ['$rootScope', '$scope', 'ApiRest','ngToast', 'urls','$timeout',
+   function ($rootScope, $scope,  ApiRest,ngToast, urls, $timeout) {
+    ApiRest.one('users/me/application').get().then(function(data) {
       $scope.me.application = data;
     });
-  ApiRest.one('users/me').get().then(function(data) {
+    ApiRest.one('users/me').get().then(function(data) {
       $scope.me = data;
     });
-    $scope.updateApplication = function()
+    var timeout = null;
+    var saveApplication = function()
     {
       ApiRest.all('users/me').customPUT($scope.me).then(function(data)
       {
         console.log(data);
         ngToast.create({
           className: 'success',
-          content: '<a>Account details updated!</a>'
+          content: '<a>Application Saved!</a>'
         });
-      })
+      });
+    }
+    
+    var debounceSaveUpdates = function(newVal, oldVal) {
+    if (newVal != oldVal) {
+      if (timeout) {
+        $timeout.cancel(timeout)
+      }
+      timeout = $timeout(saveApplication, 1000);  // 1000 = 1 second
+    }
+  };
+  $scope.$watch('me', debounceSaveUpdates, true);
+
+    $scope.updateApplication = function()
+    {
+      saveApplication();
     }
   }]);
