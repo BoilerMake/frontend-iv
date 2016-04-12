b3891a4fd1e62ce5819d87b608398db72f9cd4b0
Nicky Semenza <nicky@nickysemenza.com>
2016-04-12 00:57:19 -0400

    landing page email interest signups

---
+++ src/app/main/landing.controller.js
@@ -0,0 +1,17 @@
+angular.module('app')
+.controller('LandingController', ['$rootScope', '$scope', '$location', 'ApiRest','ngToast',
+	function($rootScope, $scope, $location, ApiRest, ngToast) {
+		$scope.subscribe = function()
+		{
+			console.log($scope.email);
+			ApiRest.all('interest/signup').customPOST({email: $scope.email}).then(function(data)
+		      {
+		        console.log(data);
+		        ngToast.create({
+		          className: 'info',
+		          content: '<a>'+data.message+'</a>'
+		        });
+		      });
+		}
+	}
+]);
