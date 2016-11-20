d72818a8b544a980206a3732b346e963002fc232
Shane DeWael <dewaels@purdue.edu>
2016-11-20 18:37:44 -0500

    adds smooth scrolling for skrollr

--- src/assets/javascripts/particle.js
+++ src/assets/javascripts/particle.js
@@ -5,7 +5,7 @@ Particles = {
   width: window.innerWidth,
   height: window.innerHeight,
   padding: 50,
-  refreshRate: 15,
+  refreshRate: 20,
   pArray: [],
   path: null,
   speedFunction: null,
