f3f8bf5f407d8d5bfae8a32cf861b16c540f9315
Nicky Semenza <nicky@nickysemenza.com>
2016-11-21 00:05:48 -0800

    test build process javascript

--- gulp/inject.js
+++ gulp/inject.js
@@ -15,6 +15,7 @@ module.exports = function(options) {
 
     var injectScripts = gulp.src([
       options.src + '/app/**/*.js',
+      options.src + '/assets/**/*.js',
       '!' + options.src + '/app/**/*.spec.js',
       '!' + options.src + '/app/**/*.mock.js'
     ])
